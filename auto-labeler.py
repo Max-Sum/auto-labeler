d4f8fe3afd1cd110b7df9bf5f79c691069a52f55
Max Sum <Max-Sum@users.noreply.github.com>
2026-07-06 16:57:23 +0800

    Process Synchronization events to label existing pods on startup

--- auto-labeler.py
+++ auto-labeler.py
@@ -23,49 +23,54 @@ print("Processing context file: ", CONTEXT_FILE)
 with open(CONTEXT_FILE) as context_hdl:
     context = json.load(context_hdl)
 
-if context[0]["type"] == "Synchronization":
-    print("Garbage fired.")
-    sys.exit(0)
-
 failed = 0
+
 for item in context:
-    obj = item["object"]
+    # Synchronization events: objects[] array with {object, filterResult}
+    # Regular events: object is pod JSON (or bare string if jqFilter active)
+    event_type = item.get("type", "")
 
-    # jqFilter stripped the object: obj is bare node name string instead of pod JSON
-    if isinstance(obj, str):
-        node_name = obj
-        # Cannot determine pod name from filtered context; skip
-        print("SKIP: jqFilter stripped pod info, only have node={}".format(node_name))
-        continue
+    if event_type == "Synchronization":
+        items = item.get("objects", [])
+    else:
+        items = [item]
 
-    node_name = obj["spec"]["nodeName"]
-    namespace = obj["metadata"]["namespace"]
-    pod_name = obj["metadata"]["name"]
-    print("Processing {}...".format(pod_name))
+    for entry in items:
+        obj = entry.get("object", entry) if isinstance(entry, dict) else entry
 
-    try:
-        node_info = json.loads(
-            subprocess.check_output(
-                ["kubectl", "get", "node", node_name, "-o", "json"]))
-    except (subprocess.CalledProcessError, json.JSONDecodeError) as e:
-        print("ERROR: Failed to get node info for {}: {}".format(node_name, e))
-        failed += 1
-        continue
+        if isinstance(obj, str):
+            # jqFilter stripped object to bare node name; skip
+            print("SKIP: jqFilter left only node={}".format(obj))
+            continue
 
-    node_labels = node_info["metadata"]["labels"]
-    copy_keys = COPY_LABELS.split(",")
+        node_name = obj["spec"]["nodeName"]
+        namespace = obj["metadata"]["namespace"]
+        pod_name = obj["metadata"]["name"]
+        print("Processing {}...".format(pod_name))
 
-    final_labels = ["auto-labeler.maxsum.io/labeled="]
-    for key in [key for key in node_labels.keys() if key in copy_keys]:
-        final_labels.append("{}={}".format(key, node_labels[key]))
-    cmd = ["kubectl", "label", "pods", pod_name, "-n", namespace, "--overwrite"] + final_labels
-    print(cmd)
-    try:
-        print(subprocess.check_output(cmd))
-    except subprocess.CalledProcessError as e:
-        print("ERROR: Failed to label pod {}: {}".format(pod_name, e))
-        failed += 1
-        continue
+        try:
+            node_info = json.loads(
+                subprocess.check_output(
+                    ["kubectl", "get", "node", node_name, "-o", "json"]))
+        except (subprocess.CalledProcessError, json.JSONDecodeError) as e:
+            print("ERROR: Failed to get node info for {}: {}".format(node_name, e))
+            failed += 1
+            continue
+
+        node_labels = node_info["metadata"]["labels"]
+        copy_keys = COPY_LABELS.split(",")
+
+        final_labels = ["auto-labeler.maxsum.io/labeled="]
+        for key in [key for key in node_labels.keys() if key in copy_keys]:
+            final_labels.append("{}={}".format(key, node_labels[key]))
+        cmd = ["kubectl", "label", "pods", pod_name, "-n", namespace, "--overwrite"] + final_labels
+        print(cmd)
+        try:
+            print(subprocess.check_output(cmd))
+        except subprocess.CalledProcessError as e:
+            print("ERROR: Failed to label pod {}: {}".format(pod_name, e))
+            failed += 1
+            continue
 
 if failed > 0:
     print("{} pod(s) failed, exiting non-zero to trigger shell-operator retry".format(failed))
